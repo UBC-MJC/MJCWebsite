0de2aa8e895c14dc6f377d8be81546dfda2d9e45
Peiyan <iforgotpy@gmail.com>
2023-04-25 02:02:32 -0700

    wrong baseurl

--- frontend/src/api/AdminAPI.ts
+++ frontend/src/api/AdminAPI.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import {baseUrl, getAuthConfig} from "./APIUtils";
 
 const getPlayersAdmin = async (authToken: string) => {
-    return axios.get(baseUrl + "/players", getAuthConfig(authToken))
+    return axios.get(baseUrl + "/admin/players", getAuthConfig(authToken))
 }
 
 export {getPlayersAdmin}
