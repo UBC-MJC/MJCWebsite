5df6a8b221c37007f29167c3c319bb4da2459c6c
Peiyan Yang <iforgotpy@gmail.com>
2023-05-02 02:31:00 -0700

    fixing create game and creating template for get game

--- backend/src/validation/game.validation.ts
+++ backend/src/validation/game.validation.ts
@@ -1,9 +1,9 @@
 import {object, string, mixed, InferType, array} from 'yup';
-import {GameType} from "@prisma/client";
+import {GameType, GameVariant} from "@prisma/client";
 
 const createGameSchema = object({
     gameType: mixed<GameType>().oneOf([GameType.RANKED, GameType.PLAY_OFF, GameType.TOURNEY]).required(),
-    gameVariant: string().oneOf(['JAPANESE', 'HONG_KONG']).required(),
+    gameVariant: mixed<GameVariant>().oneOf([GameVariant.JAPANESE, GameVariant.HONG_KONG]).required(),
     players: array().of(string().required()).length(4).required()
 })
 
--- frontend/src/game/Game.tsx
+++ frontend/src/game/Game.tsx
@@ -1,13 +1,59 @@
-import {FC} from "react";
-import {useParams} from "react-router-dom";
+import {FC, useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import {AxiosError} from "axios";
+import {getGameAPI} from "../api/GameAPI";
 
 const Game: FC = () => {
     const { id } = useParams()
+    const navigate = useNavigate();
+    const gameId = Number(id)
+
+    const [game, setGame] = useState<Game | undefined>(undefined)
+
+    useEffect(() => {
+        if (isNaN(gameId)) {
+            return
+        }
+
+        getGameAPI(gameId).then((response) => {
+            console.log("Game: ", response.data)
+            setGame(response.data)
+        }).catch((error: AxiosError) => {
+            console.log("Error fetching game: ", error.response?.data)
+            if (error.response?.status === 404) {
+                navigate("/games/not-found")
+                return
+            }
+        })
+    }, [gameId])
+
+    if (isNaN(gameId)) {
+        return (
+            <div>
+                <h1>Game Not Found</h1>
+            </div>
+        )
+    }
 
     return (
-        <div>
+        <>
             <h1>ID: {id}</h1>
-        </div>
+            <div>GameType: {game?.gameType}</div>
+            <div>GameVariant: {game?.gameVariant}</div>
+            <div>GameStatus: {game?.status}</div>
+            <h3>Players</h3>
+            {game?.players.map((player) => {
+                return (
+                    <div key={player.playerId}>{player.wind} | {player.player.username}</div>
+                )
+            })}
+            <h3>Rounds</h3>
+            {game?.rounds.map((round) => {
+                return (
+                    <div key={round.id}>{round.roundWind} | {round.roundNumber} | {round.roundCount}</div>
+                )
+            })}
+        </>
     )
 }
 
