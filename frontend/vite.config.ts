b566ba8af2c1469d461e9693647b1485feb146f0
Zihao Huang <36803666+ZihaoHuang2017@users.noreply.github.com>
2024-07-11 14:49:18 -0700

    Migrate to vite (#55)
    * migrate to vite as cra is deprecated
    
    * revert eslintrc change
    
    * change tsconfig

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        // this ensures that the browser opens upon server start
+        open: false,
+        // this sets a default port to 3000
+        port: 3000,
+    },
+})
+
