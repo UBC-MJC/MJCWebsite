c66535e1130f457adb84cb1330621288f58e29a6
Peiyan <iforgotpy@gmail.com>
2025-03-02 18:10:00 -0800

    add position-sticky

--- frontend/src/game/jp/legacy/LegacyJapaneseGame.tsx
+++ frontend/src/game/jp/legacy/LegacyJapaneseGame.tsx
@@ -399,7 +399,7 @@ const LegacyJapaneseGame: FC<LegacyGameProps> = ({
                 rounds={mapRoundsToModifiedRounds(game.rounds as JapaneseRound[])}
                 players={players}
             />
-            <Container fluid className={"my-4 bottom-0 bg-body"}>
+            <Container fluid className={"my-4 bottom-0 bg-body position-sticky"}>
                 <Row className={"row-cols-4 align-items-end"}>
                     {getScoresWithPlayers(game, "jp").map(({ username, score }, idx) => (
                         <Col key={idx} className={"my-2"}>
