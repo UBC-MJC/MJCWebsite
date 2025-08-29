3dab8b63a33b05a69e38d1c1399ba2e571482c93
Zihao Huang <zihaohuang2017@gmail.com>
2025-08-29 11:53:56 +0900

    Remove VRO propaganda

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -6,10 +6,6 @@ const Home: FC = () => {
     return (
         <div className={"my-4"}>
             <h1>Home</h1>
-            <h2>
-                Register for <a href="https://forms.gle/sw9CnSsokeyJB6pK6">Vancouver Riichi Open</a>{" "}
-                today!
-            </h2>
             <img src={logo} className={"w-50"} alt={"Logo of UBC MJC"} />
             <p>Club room location: Room 3206B, The Nest</p>
             <p>The University of British Columbia, Vancouver, BC</p>
