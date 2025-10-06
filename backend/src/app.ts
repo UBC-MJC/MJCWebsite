eb3cf48c25e046b8380ce79fb83000354c3eea9f
Peiyan <iforgotpy@gmail.com>
2025-10-05 18:42:17 -0700

    Simplify app.ts logic after nginx update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,6 @@ import bodyParser from "body-parser";
 import router from "./routes";
 import path from "path";
 import { Player } from "@prisma/client";
-import * as fs from "fs";
-import * as https from "https";
 
 // Load environment-specific .env file
 const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
@@ -52,27 +50,9 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     next();
 });
 
-const DEV_PORT: string | number = process.env.PORT || 4000;
-const HTTP_PORT: number = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 8080;
-const HTTPS_PORT: number = process.env.HTTPS_PORT ? parseInt(process.env.HTTPS_PORT) : 8443;
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : process.env.NODE_ENV === "production" ? 8080 : 4000;
 
-if (process.env.NODE_ENV === "production") {
-    const httpApp = express();
-    httpApp.get("*", (req, res) => {
-        res.redirect("https://" + req.headers.host + req.url);
-    });
-    httpApp.listen(HTTP_PORT, () => {
-        console.log(`HTTP Server running on port ${HTTP_PORT}`);
-    });
-
-    const privateKey = fs.readFileSync(path.join(__dirname, "../../certificate/mjcserver.key"));
-    const certificate = fs.readFileSync(path.join(__dirname, "../../certificate/_.ubc.gg.crt"));
-    const ca = fs.readFileSync(path.join(__dirname, "../../certificate/GandiCert.pem"));
-
-    const credentials = { key: privateKey, cert: certificate, ca: ca };
-    https.createServer(credentials, app).listen(HTTPS_PORT, () => {
-        console.log(`HTTPS Server running on port ${HTTPS_PORT}`);
-    });
-} else {
-    app.listen(DEV_PORT, () => console.log(`Server running on http://localhost:${DEV_PORT}`));
-}
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`);
+});
